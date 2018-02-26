e0678143613669cbe174a3188e7142e73032b156
Alejandro Anzola <anzola95avila@gmail.com>
2018-02-26 17:03:12 -0500

    responde con los header para ser correctamente interpretado por el navegador

--- src/main/java/edu/eci/arem/tallerclientesyservicios/ejercicio44/HttpServer.java
+++ src/main/java/edu/eci/arem/tallerclientesyservicios/ejercicio44/HttpServer.java
@@ -37,7 +37,21 @@ public class HttpServer {
             }
         }
 
-        outputLine = "<!DOCTYPE html>\n"
+        outputLine = "HTTP/1.1 200 OK\n"
+                + "Content-Type: text/html; charset=utf-8\n"
+                + "Cache-Control: public, max-age=60, s-maxage=300\n"
+                + "Vary: Accept-Encoding\n"
+                + "Content-Encoding: raw\n"
+                + "Server: DPS/1.3.5\n"
+                + "X-SiteId: 2000\n"
+                + "Set-Cookie: dps_site_id=2000; path=/\n"
+                + "ETag: a1be084ec13bc207c17c49955f875fab\n"
+                + "Date: Mon, 26 Feb 2018 21:47:54 GMT\n"
+                + "Connection: keep-alive\n"
+                + "Transfer-Encoding: chunked\n"
+                + "\n"
+                + "3c6f"
+                + "<!DOCTYPE html>\n"
                 + "<html>\n"
                 + "  <head>\n"
                 + "    <meta charset=\"UTF-8\" />\n"
@@ -46,8 +60,11 @@ public class HttpServer {
                 + "  <body>\n"
                 + "    <h1>My website</h1>\n"
                 + "  </body>\n"
-                + "</html>" + inputLine;
+                + "</html>\n"
+                + inputLine;
         out.println(outputLine);
+
+        System.out.println(">>>\"" + inputLine + "\"");
 
         out.close();
         in.close();
